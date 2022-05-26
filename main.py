2e6feb94e9dd1c8842896a151b042259777b15e2
Prince Gupta <princegupta1586@gmail.com>
2022-05-26 15:48:54 +0530

    inital commit

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "princeviditauditaakshay"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://qhyjefvjdfateb:4b0b52435975215b716538567b04ca608da37709122a30e20a40e9bd218d48a7@ec2-54-211-255-161.compute-1.amazonaws.com:5432/dbjjl39edi35kd"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Bootstrap(app)
